ddbe3a7e9f11a7c227fe0fcf8ebc914faecf339e
Shashank <shashank08090@gmail.com>
2022-09-16 23:27:06 +0530

    fixed cors issue

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
-const express = require ('express');
-const mongoose = require('mongoose');
-require('./db/config')
-const User = require('./db/User')
-const app = express()
-app.use(express.json())
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("./db/config");
+const User = require("./db/User");
+const app = express();
+app.use(cors());
+// app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.post("/register", async (req, res) => {
+  let user = new User(req.body);
+  let result = await user.save();
+  console.log(user);
+  res.send([req.body]);
+  res.end();
+  console.log("sign up ");
+});
+app.post("/login", async (req, res) => {
+  let user = await User.findOne(req.body);
+  console.log(req.body);
 
-app.post('/register', async(req,res) =>{
-    let user = new User (req.body)
-    let result = await user.save()
-   console.log(user)
-   res.send(req.body)
-   res.end()
-   console.log('sign up ')
-})
-app.post('/login', async(req, res) => {
-let user = await User.findOne(req.body)
-if(user) {
-   res.send('user found, you are successfully logged in')
-}else {
-   res.send('login failed, please check your name and password')
-}
+  if (user) {
+    res.send({ user });
+  } else {
+    res.send({ msg: "failed" });
+  }
 });
 //  const connectdb = async() =>{
 //     mongoose.connect('mongodb://localhost:27017/myproject', ()=> console.log('db connected'))
@@ -30,5 +37,5 @@ if(user) {
 //     console.warn(product)
 //  }
 //  connectdb()
-app.get('/' , (req,res) => res.send("hey ! it is working"))
-app.listen(5000 , ()=> console.log("the app is working"))
+app.get("/", (req, res) => res.send("hey ! it is working"));
+app.listen(5000, () => console.log("the app is working"));
