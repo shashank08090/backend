ea17f46758bbbc875157a478679a06c2fc32c3bb
Shashank <shashank08090@gmail.com>
2022-09-25 01:35:18 +0530

    question api tested- working successfully

---
+++ db/questions.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const questionSchema = new mongoose.Schema({
+  name: String,
+  question: String,
+});
+
+questionSchema.index({ name: "text" });
+module.exports = mongoose.model("question", questionSchema);
