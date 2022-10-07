508eee725693f4f2438257cae35e1c5170d26d16
Shashank <shashank08090@gmail.com>
2022-10-07 19:11:01 +0530

    dislike feature

--- db/like.js
+++ db/like.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const likeSchema = new mongoose.Schema({
   question: String,
   likedBy: [],
+  dislikedBy: [],
 });
 
 module.exports = mongoose.model("likes", likeSchema);
